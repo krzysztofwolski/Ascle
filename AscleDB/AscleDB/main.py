07d48b08fbcdd442b430e3de3d34a6c4a596bc1b
Krzysztof Wolski <krzysztof.k.wolski@gmail.com>
2013-10-10 19:56:50 +0200

    Some changes to models.

--- AscleDB/AscleDB/main.py
+++ AscleDB/AscleDB/main.py
@@ -1,7 +1,7 @@
 import flask
 import flask.ext.sqlalchemy
 import flask.ext.restless
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Boolean
+from sqlalchemy import Column, Integer, String, ForeignKey, Date, DateTime, Boolean
 from sqlalchemy.orm import relationship, backref
 
 app = flask.Flask(__name__)
@@ -14,53 +14,56 @@ db = flask.ext.sqlalchemy.SQLAlchemy(app)
 class User(db.Model):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    login = Column(String(50), unique=True)
-    password = Column(String(50))
-    last_name = Column(String(50))
-    first_name = Column(String(120))
-    email = Column(String(120), unique=True)
-    type = Column(Integer)
+    login = Column(String(50), unique=True, nullable=False)
+    password = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    first_name = Column(String(120), nullable=False)
+    type = Column(Integer, nullable=False)
+
 
 class Measure(db.Model):
     __tablename__ = 'measure'
     id = Column(Integer, primary_key=True)
-    sensor_id = Column(Integer, ForeignKey('sensor.id'))
-    value = Column(Integer)
+    sensor_id = Column(Integer, ForeignKey('sensor.id'), nullable=False)
+    value = Column(Integer, nullable=False)
     timestamp = Column(DateTime)
+
 
 class Drug(db.Model):
     __tablename__ = 'drug'
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    name = Column(String(50))
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    name = Column(String(50), nullable=False)
     dose = Column(String(50))
+
 
 class Sensor(db.Model):
     __tablename__ = 'sensor'
     id = Column(Integer, primary_key=True)
-    name = Column(String(50), unique=True)
-    sensor_type_id = Column(Integer, ForeignKey('sensortype.id'))
+    name = Column(String(50), unique=True, nullable=False)
+    sensor_type_id = Column(Integer, ForeignKey('sensortype.id'), nullable=False)
     alert_min = Column(Integer)
     alert_max = Column(Integer)
     warning_min = Column(Integer)
     warning_max = Column(Integer)
-    user_id = Column(Integer, ForeignKey('user.id'))
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+
 
 class SensorType(db.Model):
     __tablename__ = 'sensortype'
     id = Column(Integer, primary_key=True)
-    name = Column(String(50))
+    name = Column(String(50), unique=True, nullable=False)
     unit = Column(String(10))
-    automatic = Column(Boolean)
+    automatic = Column(Boolean, nullable=False)
 
 
 class Messages(db.Model):
     __tablename__ = 'messages'
     id = Column(Integer, primary_key=True)
-    sender_user_id = Column(Integer, ForeignKey('user.id'))
-    receiver_user_id = Column(Integer, ForeignKey('user.id'))
-    text = Column(String(500))
-    new = Column(Boolean)
+    sender_user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    receiver_user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    text = Column(String(500), nullable=False)
+    new = Column(Boolean, nullable=False)
 
 
 class Schedule(db.Model):
@@ -71,8 +74,8 @@ class Schedule(db.Model):
     afternoon_dose = Column(Boolean)
     midnight_dose = Column(Boolean)
     time_interval = Column(Integer)
-    date_start = Column(DateTime)
-    date_stop = Column(DateTime)
+    date_start = Column(Date, nullable=False)
+    date_stop = Column(Date)
     sensor_id = Column(Integer, ForeignKey('sensor.id'))
     drug_id = Column(Integer, ForeignKey('drug.id'))
 
