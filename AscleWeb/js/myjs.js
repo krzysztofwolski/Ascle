f29be61a8f96cff9e7d3b9aca50bfd4c30d4029d
marekromanski <mromanski91@gmail.com>
2013-10-17 18:37:47 +0200

    poprawki

--- AscleWeb/js/myjs.js
+++ AscleWeb/js/myjs.js
@@ -21,26 +21,19 @@
 		});
 
 		
-		
 	});
 	$("#showpic_2").on('click',function()
 	{
 		var api = "http://kuchnia.mooo.com:5000/api/user";
-		$.getJSON(api,	
+		$.getJSON(api, {'Content-Type': 'application/json'}, function(result)
 		{
-		    tagmode: "any",
-		    format: "json"
-	    }).done(function( data ) 
-    	{
-  			$.each( data.items, function( i, item ) 
-  			{
-		        $( "<img>" ).attr( "src", item.media.m ).appendTo( "#images" );
-		        if ( i === 3 ) 
-		        {
-		        	return false;
-       	 		}
-      		});
-		});
+			$("#showpic_2").text("aaa");
+			console.log("aaa");
+	    	/*$.each(result, function(i, field)
+	    	{
+	      		$("div").append(field + " ");
+    		});*/
+ 		});
 	});
 
 });
