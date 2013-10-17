909f5ccdac6f1263273de28ae2edf150e60b2d5e
marekromanski <mromanski91@gmail.com>
2013-10-17 18:02:31 +0200

    wyswietlanie obrazkow na podstawie JSONa

--- AscleWeb/js/myjs.js
+++ AscleWeb/js/myjs.js
@@ -1,6 +1,47 @@
 ﻿$(document).ready(function() {
-	$("#showpic_1").on('click',function(){
+	$("#showpic_1").on('click',function()
+	{
 		$('#showpic_1').hide();
+		var api = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+		$.getJSON(api,	
+		{
+		    tags: "mount rainier",
+		    tagmode: "any",
+		    format: "json"
+	    }).done(function( data ) 
+    	{
+  			$.each( data.items, function( i, item ) 
+  			{
+		        $( "<img>" ).attr( "src", item.media.m ).appendTo( "#images" );
+		        if ( i === 3 ) 
+		        {
+		        	return false;
+       	 		}
+      		});
+		});
+
+		
+		
 	});
+	$("#showpic_2").on('click',function()
+	{
+		var api = "http://kuchnia.mooo.com:5000/api/user";
+		$.getJSON(api,	
+		{
+		    tagmode: "any",
+		    format: "json"
+	    }).done(function( data ) 
+    	{
+  			$.each( data.items, function( i, item ) 
+  			{
+		        $( "<img>" ).attr( "src", item.media.m ).appendTo( "#images" );
+		        if ( i === 3 ) 
+		        {
+		        	return false;
+       	 		}
+      		});
+		});
+	});
+
 });
 
