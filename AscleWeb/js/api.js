f8958fc7d9d226d924be7bf2b9f5a26c00cf4c4e
KrzychuJedi <krzychu224@o2.pl>
2013-10-21 08:57:56 +0200

    Added files

---
+++ AscleWeb/js/api.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function() {
+	
+	$("#2").on('click',function(){
+		console.log("clicked");
+		
+		var api = "http://kuchnia.mooo.com:5000/api/user/1";
+		$.getJSON(api, {'Content-Type': 'application/json'}, function(result)
+		{
+			$("#showpic_2").text("aaa");
+			console.log(result.first_name);
+			console.log($(".test").text());
+			$(".test").text($(".test").text() + " " + result.first_name);
+			
+				/*$.each(result, function(i, field)
+	    	{
+	      		$("div").append(field + " ");
+    		});*/
+ 		});
+			
+	
+	});
+	
+	
+});
+
